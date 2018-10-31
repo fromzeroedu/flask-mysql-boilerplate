fbd68e1a31f5ebe12009e5685ca88cc4486affb1
numanabd123 <44594749+numanabd123@users.noreply.github.com>
2018-10-31 10:13:09 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 import os
 
 SECRET_KEY=os.environ['SECRET_KEY']
-DB_USERNAME=os.environ['DB_USERNAME']
-DB_PASSWORD=os.environ['DB_PASSWORD']
-DB_HOST=os.environ['DB_HOST']
-DATABASE_NAME=os.environ['DATABASE_NAME']
-DB_URI = "mysql+pymysql://%s:%s@%s:3306/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_NAME)
+DB_USERNAME=os.environ['cloudpagi01']
+DB_PASSWORD=os.environ['pwcloudpagi01']
+DB_HOST=os.environ['103.101.225.51']
+DATABASE_NAME=os.environ['dbcloudpagi']
+DB_URI = "mysql+pymysql://%s:%s@%s:3306/%s" % (cloudpagi01, pwcloudpagi01, 103.101.225.51, dbcloudpagi)
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 MYSQL_ROOT_PASSWORD=os.environ['MYSQL_ROOT_PASSWORD']
